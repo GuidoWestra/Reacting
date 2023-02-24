ff7cf1390ec6a814a5863e97fc39044ab1928fae
GuidoWestra <westra.guido@gmail.com>
2023-02-24 01:28:20 +0100

    Moving Logo's

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="App">
+  <div >
     <React.StrictMode>
       <BrowserRouter>
         <App />
