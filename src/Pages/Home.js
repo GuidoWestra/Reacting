435624ef5ffa08424d2107ab37a42b41a1c3930f
GuidoWestra <westra.guido@gmail.com>
2023-03-02 15:21:15 +0100

    Made a first iteration of the project w/ urs.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
 
 export default function Home() {
+    let coreA = ["Guido", "Urs", "Remy", "Sjoerd" ];
+    const [num, setNum] = useState(0);  
+
+    const handleClick = () => {
+        console.log('hi')
+        setNum(num+1);
+    };
+    
+    useEffect(() => {    
+        console.log('Incrementing to',num)
+
+    }, [num])
+    
     return (
         <div >
             <div>
-                Landing
+                <Alert key="dark" variant="dark">
+                    {coreA[num]}
+                </Alert>
+
+
+
+                <Button onClick={handleClick} variant="dark">Dark</Button>
             </div>
         </div >
     )
